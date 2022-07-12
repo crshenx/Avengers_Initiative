dd5c28fba36e4d59074ab330b44f9687e5f76cf4
Charles Jarvis <csjarvis43@gmail.com>
2022-07-12 10:06:13 -0600

    cards size now stable

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,53 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Cards({ hero }) {
-  const {name, thumbnail, description, urls, comics} = hero
+  const {name, thumbnail, description, urls, comics, resourceURI} = hero
 
+  const [showCard, setShowCard] = useState(true)
 
 
   console.log(hero)
+
   return (
 <div className='media-content'>
-    <div className="card">
-        <header class="card-header">
-        <p class="card-header-title" id="toggle_shown">
-        {name}
-        </p>
-        <button class="card-header-icon" aria-label="more options">
-        <span class="icon">
-            <i class="fas fa-angle-down" aria-hidden="true"></i>
-        </span>
-        </button>
-    </header>
-    <div className="card-image" >
-        <figure className="image is-4by3">
-        <img src={thumbnail.path + "." + thumbnail.extension} alt="hero thumbnail"/>
-        </figure>
-    </div>
-    <div className="card-content">
-        <div className="media">
-        <div className="media-left">
-            <figure className="image is-48x48">
-            <img src={thumbnail.path + "." + thumbnail.extension} alt="Main hero profile pic"/>
+    <div>
+        <div className="card">
+            <header class="card-header">
+            <p class="card-header-title" id="toggle_shown">
+            {name}
+            </p>
+            <button class="card-header-icon" aria-label="more options">
+            <span class="icon">
+                <i class="fas fa-angle-down" aria-hidden="true"></i>
+            </span>
+            </button>
+        </header>
+        <div className="card-image" >
+            <figure className="image is-4by3">
+            <img src={thumbnail.path + "." + thumbnail.extension} alt="hero thumbnail"/>
             </figure>
         </div>
-        <div className="media-content">
-            <p className="title is-4">{name}</p>
-            <a href={urls[0].url}>Details</a>
-            <br/>
-            <a href={comics.collectionURI}>Comics</a>
-        </div>
-        </div>
+        <div className="card-content">
+            <div className="media">
+            <div className="media-left">
+                <figure className="image is-48x48">
+                <img src={thumbnail.path + "." + thumbnail.extension} alt="Main hero profile pic"/>
+                </figure>
+            </div>
+            <div className="media-content">
+                <p className="title is-4">{name}</p>
+                <a href={urls[0].url}>Details</a>
+                <br/>
+                <a href={comics.collectionURI}>Comics</a>
+            </div>
+            </div>
 
-        <div className="content">
-            {description}
+            <div className="content">
+                {description !== "" ? description : "No Description Available, Click the details link for more information about his hero"}
+            </div>
+            <footer class="card-footer">
+            <button href="#" class="card-footer-item">Team Up!</button>
+            <button href="#" class="card-footer-item">Dismiss</button>
+        </footer>
         </div>
-        <footer class="card-footer">
-        <button href="#" class="card-footer-item">Team Up!</button>
-        <button href="#" class="card-footer-item">Dismiss</button>
-    </footer>
+        </div> 
     </div>
-    </div> 
 </div>
   )
 }

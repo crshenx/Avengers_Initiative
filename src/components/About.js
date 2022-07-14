7be4653e18fd9a9bd2746b5df612251a01fdfbc3
Charles Jarvis <csjarvis43@gmail.com>
2022-07-14 11:27:35 -0600

    began work on about us cards

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,59 @@
 import React from 'react'
+import Grid from '@mui/material/Grid'
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import { Typography } from '@mui/material'
+
 
 function About() {
   return (
-    <div>About</div>
+    <div>
+        <Box sx={{ flexGrow: 1}}>
+            <Grid container spacing={2} columns={10}>
+                <Grid item xs={5}>
+                    <Card>
+                    <CardContent>
+                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                        Word of the Day
+                        </Typography>
+                        <Typography variant="h5" component="div">
+                        poop
+                        </Typography>
+                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                        adjective
+                        </Typography>
+                        <Typography variant="body2">
+                        well meaning and kindly.
+                        <br />
+                        {'"a benevolent smile"'}
+                        </Typography>
+                    </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs={5}>
+                    <Card>
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                            Word of the Day
+                            </Typography>
+                            <Typography variant="h5" component="div">
+                            poop
+                            </Typography>
+                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                            adjective
+                            </Typography>
+                            <Typography variant="body2">
+                            well meaning and kindly.
+                            <br />
+                            {'"a benevolent smile"'}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Box>
+    </div>
   )
 }
 

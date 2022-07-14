b6f347a3c5b1d2202ca91a2edd971226cfb798bb
Charles Jarvis <csjarvis43@gmail.com>
2022-07-14 13:44:05 -0600

    about us halfway finished

--- src/components/About.js
+++ src/components/About.js
@@ -3,52 +3,98 @@ import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
 import { Typography } from '@mui/material'
+import Link from '@mui/material/Link'
+import './About.css'
 
 
 function About() {
+
+    const cardStyle = {
+        display: 'block',
+        height: '45vh',
+        alignItems: 'center'
+    }
+
+
+
+
+
   return (
     <div>
         <Box sx={{ flexGrow: 1}}>
             <Grid container spacing={2} columns={10}>
                 <Grid item xs={5}>
-                    <Card>
+                    <Card className="aboutCard" style={cardStyle}>
                     <CardContent>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                        Word of the Day
+                        <Typography sx={{ fontSize: 22 }} align='center' color="text.secondary" gutterBottom>
+                        Developer
                         </Typography>
-                        <Typography variant="h5" component="div">
-                        grin
+                        <Typography align='center' variant="h3" component="div">
+                        Charles Jarvis
                         </Typography>
-                        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                        adjective
+                        <Typography sx={{ mb: 1.5 }} align='center' color="text.secondary">
+                        biography
                         </Typography>
-                        <Typography variant="body2">
-                        well meaning and kindly.
+                        <Typography align='center' variant="body1">
+                        Former service industry worker turned software engineer. Lifelong interest in computers which I hope to
+                        turn into a lifelong career. Currently enrolled in the Flatiron school, learning JavaScript, React, Ruby,
+                        and Ruby on rails. Prior to enrolling in Flatiron, received my degree in Computer Networking and Virtualization 
+                        from Front Range Community College in Colorado. 
                         <br />
-                        {'"a benevolent smile"'}
+                        <br />
+                        Below are Charles' relevant links:
+                        </Typography>
+                        <br />
+                        <Typography align='center'>
+                            <Link variant="h4" href="https://github.com/CSJarvis43" color="primary">
+                                GitHub
+                            </Link>
+                        </Typography>
+                        <br />
+                        <Typography align='center'>
+                            <Link variant="h4" href="https://www.linkedin.com/in/charles-jarvis-539b2314a/" color="primary">
+                                LinkedIn
+                            </Link>
                         </Typography>
                     </CardContent>
                     </Card>
                 </Grid>
                 <Grid item xs={5}>
-                    <Card>
+                    <Card  className="aboutCard" style={cardStyle}>
                         <CardContent>
-                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            Word of the Day
+                            <Typography sx={{ fontSize: 22 }} align='center' color="text.secondary" gutterBottom>
+                            Developer
                             </Typography>
-                            <Typography variant="h5" component="div">
-                            grin
+                            <Typography variant="h3" align='center' component="div">
+                            Chris Hendricks
                             </Typography>
-                            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-                            adjective
+                            <Typography sx={{ mb: 1.5 }} align='center' color="text.secondary">
+                            biography
                             </Typography>
-                            <Typography variant="body2">
+                            <Typography align='center' variant="body1">
                             well meaning and kindly.
                             <br />
-                            {'"a benevolent smile"'}
+                            <br />
+                            Below are Chris' relevant links:
                             </Typography>
+                            <br />
+                            <Typography align='center'>
+                            <Link variant="h4" href="https://github.com/crshenx" color="primary">
+                                GitHub
+                            </Link>
+                        </Typography>
+                        <br />
+                        <Typography align='center'>
+                            <Link variant="h4" href="https://www.linkedin.com/in/chris-hendricks-109b0476/" color="primary">
+                                LinkedIn
+                            </Link>
+                        </Typography>
                         </CardContent>
+                        <CardMedia>
+
+                        </CardMedia>
                     </Card>
                 </Grid>
             </Grid>
